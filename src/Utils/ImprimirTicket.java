d93b64e792492fd108d1c04b13587e7fb03fa959
Diego <diego.mayorga86@gmail.com>
2016-07-15 19:00:07 -0300

    clean code

--- src/Utils/ImprimirTicket.java
+++ src/Utils/ImprimirTicket.java
@@ -13,31 +13,30 @@ import javax.print.SimpleDoc;
 public class ImprimirTicket {
     public static int printTicket (String ticket) {
         int result = 0;
+        String ticketToByte;
+        //Cogemos el servicio de impresion por defecto (impresora por defecto)
+        PrintService service = PrintServiceLookup.lookupDefaultPrintService();
+        //Le decimos el tipo de datos que vamos a enviar a la impresora
+        //Tipo: bytes Subtipo: autodetectado
+        DocFlavor flavor = DocFlavor.BYTE_ARRAY.AUTOSENSE;
+        //Creamos un trabajo de impresion
+        DocPrintJob pj = service.createPrintJob();
+        //Nuestro trabajo de impresion envia una cadena de texto
+        ticketToByte = new String(ticket);
+        byte[] bytes;
+        //Transformamos el texto a bytes que es lo que soporta la impresora
+        bytes = ticketToByte.getBytes();
+        //Creamos un documento (Como si fuese una hoja de Word para imprimir)
+        Doc doc = new SimpleDoc(bytes,flavor,null);
+        //PrintRequestAttributeSet aset = new HashPrintRequestHashAttributeSet();
+        //aset.add(MediaSizeName.ISO_A4);
+        //Obligado coger la excepcion PrintException
+        //Mandamos a impremir el documento
+        //PrintService[] pservices =PrintServiceLookup.lookupPrintServices(flavor, null);
         try {
-            String ticketToByte;
-            //ticket = cadena + cade + "\nTOTAL: \t\t\t\t"+totalVentas.getText()+"\n\nEfectivo: \t\t\t"+efectivo+"\nDescuento: \t\t\t"+descuento+"\nCambio: \t\t\t"+cambio+"\n\nLe atendio: "+nombre+" "+apellido+"\n\nGRACIAS POR SU COMPRA.\n\n\n\n";
-            //Cogemos el servicio de impresion por defecto (impresora por defecto)
-            PrintService service = PrintServiceLookup.lookupDefaultPrintService();
-            //Le decimos el tipo de datos que vamos a enviar a la impresora
-            //Tipo: bytes Subtipo: autodetectado
-            DocFlavor flavor = DocFlavor.BYTE_ARRAY.AUTOSENSE;
-            //Creamos un trabajo de impresion
-            DocPrintJob pj = service.createPrintJob();
-            //Nuestro trabajo de impresion envia una cadena de texto
-            ticketToByte = ticket;
-            byte[] bytes;
-            //Transformamos el texto a bytes que es lo que soporta la impresora
-            bytes = ticketToByte.getBytes();
-            //Creamos un documento (Como si fuese una hoja de Word para imprimir)
-            Doc doc=new SimpleDoc(bytes,flavor,null);
-            //PrintRequestAttributeSet aset = new HashPrintRequestHashAttributeSet();
-            //aset.add(MediaSizeName.ISO_A4);
-            //Obligado coger la excepcion PrintException
-            //Mandamos a impremir el documento
-            PrintService[] pservices =PrintServiceLookup.lookupPrintServices(flavor, null);
             pj.print(doc, null);
-            System.out.println(ticket);
         } catch (PrintException ex) {
+            System.out.println("Error al imprimir: "+ex.getMessage());
             Logger.getLogger(ImprimirTicket.class.getName()).log(Level.SEVERE, null, ex);
             return result = 2;
         }
