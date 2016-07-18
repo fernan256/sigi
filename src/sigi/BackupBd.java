f664df8fe381c448def59f0df9148c819bfb5349
Diego <diego.mayorga86@gmail.com>
2016-07-18 19:24:39 -0300

    fix restore function

--- src/sigi/BackupBd.java
+++ src/sigi/BackupBd.java
@@ -1,6 +1,7 @@
 package sigi;
 
 import Connection.Conexion;
+import java.awt.HeadlessException;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
@@ -19,7 +20,11 @@ public class BackupBd extends javax.swing.JInternalFrame {
     ResultSet rs;
     JFileChooser chooser = new JFileChooser();
     Conexion mysql;
+    String user = "root";
+    String password = "dedox132";
+    String bd = "sigi";
     String backup = "";
+    String dbName = "sigi";
     private String fecha;
     
     public BackupBd() {
@@ -123,7 +128,7 @@ public class BackupBd extends javax.swing.JInternalFrame {
                     .addComponent(examinFile)
                     .addComponent(restaurarBd, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(restoreRout, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(455, Short.MAX_VALUE))
+                .addContainerGap(459, Short.MAX_VALUE))
         );
 
         pack();
@@ -134,53 +139,53 @@ public class BackupBd extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_respaldoBdActionPerformed
 
     private void restaurarBdActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_restaurarBdActionPerformed
-
-            if(restoreRout.getText().length() > 0){
-                try{
-                    File f;
-                    f = new File(restoreRout.getText());
-                    String path = f.getAbsolutePath();
-                    int c = JOptionPane.showConfirmDialog(null, "Restaurar BD", "Confirmacion", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
-                    if(c == JOptionPane.YES_OPTION){
-                        mysql = new Conexion();
-                        mysql.Comandos("drop table ajuste_caja, anular_ticket, caja, cierre_caja, cuentas_corrientes, clientes, movimiento_stock, oc_productos, ordenes_compra, devoluciones, detalle_ajuste_stock, detalle_venta, ventas, descripcion_articulos, stock, proveedores, retiro_dinero;");
-                        //mysql = new Conexion();
-                        //mysql.Comandos("create database sigi;");
-                        mysql = new Conexion();
-                        backup = "cmd /c mysql --user=root --password=dedox132 sigi < " + path;
-                        Process child = Runtime.getRuntime().exec(backup);
-                        JOptionPane.showMessageDialog(null, "ok");
-                    }
-                } catch (ClassNotFoundException ex) {
-                    Logger.getLogger(BackupBd.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (SQLException ex) {
-                    Logger.getLogger(BackupBd.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (InstantiationException ex) {
-                    Logger.getLogger(BackupBd.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IllegalAccessException ex) {
-                    Logger.getLogger(BackupBd.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException ex) {
-                    Logger.getLogger(BackupBd.class.getName()).log(Level.SEVERE, null, ex);
+        if(restoreRout.getText().length() > 0){
+            try{
+                File f;
+                f = new File(restoreRout.getText());
+                String path = f.getAbsolutePath();
+                Object[] options = {"Aceptar", "Cancelar"};
+                int routeToRestore = JOptionPane.showOptionDialog(null, "Confirmar Base de Datos: "+restoreRout.getText(), "Restaurar Base de Datos", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[0]);
+                if(routeToRestore == 0){
+                    //mysql.Comandos("drop table ajuste_caja, anular_ticket, caja, cierre_caja, cuentas_corrientes, clientes, movimiento_stock, oc_productos, ordenes_compra, devoluciones, detalle_ajuste_stock, detalle_venta, ventas, descripcion_articulos, stock, proveedores, retiro_dinero;");
+                    mysql = new Conexion();
+                    mysql.Comandos("drop database if exists sigi;");
+                    mysql.Comandos("create database sigi;");
+                    restoreDB(dbName, user, password, path);
+                    clearBackUpFields();
+                    mysql.Cerrar();
+                }else{
+                    JOptionPane.showMessageDialog(null, "Se cancelo la operación");
+                    clearBackUpFields();
                 }
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(BackupBd.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(BackupBd.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (InstantiationException ex) {
+                Logger.getLogger(BackupBd.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                Logger.getLogger(BackupBd.class.getName()).log(Level.SEVERE, null, ex);
             }
+        }
     }//GEN-LAST:event_restaurarBdActionPerformed
 
     private void examinFileActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_examinFileActionPerformed
         JFileChooser select = new JFileChooser(System.getProperty("user.dir"));
-        select.setDialogTitle("Abrir Qyery");
+        select.setDialogTitle("Seleccionar ruta");
         File f;
         int returnVal = select.showOpenDialog(null);
         if(returnVal == JFileChooser.APPROVE_OPTION){
             f = select.getSelectedFile();
             String cad = f.getAbsolutePath();
-            restoreRout.setText(cad+"_"+fecha+".sql");
+            restoreRout.setText(cad);
         }
         
     }//GEN-LAST:event_examinFileActionPerformed
 
     private void selectFolderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_selectFolderActionPerformed
         JFileChooser select = new JFileChooser(System.getProperty("user.dir"));
-        select.setDialogTitle("Abrir Qyery");
+        select.setDialogTitle("Seleccionar ruta");
         File f;
         int returnVal = select.showOpenDialog(null);
         if(returnVal == JFileChooser.APPROVE_OPTION){
@@ -191,8 +196,8 @@ public class BackupBd extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_selectFolderActionPerformed
     private void createBackup () {
         Object[] options = {"Aceptar", "Cancelar"};
-        int routeOk = JOptionPane.showOptionDialog(null, "Confirmar ruta: "+saveRoute.getText(), "Respaldo de Base de Datos", JOptionPane.PLAIN_MESSAGE, JOptionPane.WARNING_MESSAGE, null, options, options[0]);
-        if(routeOk == 0){
+        int routeToSave = JOptionPane.showOptionDialog(null, "Confirmar ruta: "+saveRoute.getText(), "Respaldo de Base de Datos", JOptionPane.PLAIN_MESSAGE, JOptionPane.WARNING_MESSAGE, null, options, options[0]);
+        if(routeToSave == 0){
             try{
                 con = new Conexion();
                 Runtime runtime = Runtime.getRuntime();
@@ -217,7 +222,25 @@ public class BackupBd extends javax.swing.JInternalFrame {
             clearBackUpFields();
         }else{
             JOptionPane.showMessageDialog(null, "Se cancelo la operación");
+            clearBackUpFields();
         }
+    }
+    public static boolean restoreDB(String dbName, String dbUserName, String dbPassword, String source) {
+        String[] executeCmd = new String[]{"C:\\Program Files\\MySQL\\MySQL Server 5.7\\bin\\mysql", "--user=" + dbUserName, "--password=" + dbPassword, dbName,"-e", " source "+source};  
+        Process runtimeProcess;  
+        try { 
+            runtimeProcess = Runtime.getRuntime().exec(executeCmd);  
+            int processComplete = runtimeProcess.waitFor();  
+            if (processComplete == 0) {  
+                JOptionPane.showMessageDialog(null, "Restauración correcta de la Base de Datos "+source);
+                return true;  
+            } else {  
+                JOptionPane.showMessageDialog(null, "No se pudo realizar la restauración de la Base de Datos "+source);
+            }  
+        } catch (IOException | InterruptedException | HeadlessException ex) {
+            ex.printStackTrace();
+        }  
+        return false;  
     }
     public void clearBackUpFields(){
         restoreRout.setText("");
