9cefade6f0072059866e3042b16f63a226d33db4
Diego <diego.mayorga86@gmail.com>
2016-07-18 19:22:27 -0300

    add new SQL conextion for user only

---
+++ src/Connection/Conexion_login.java
@@ -0,0 +1,32 @@
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Conexion_login {
+    private Connection con;
+    private Statement sentSQL;
+    private ResultSet rst;
+    public Conexion_login() throws ClassNotFoundException,SQLException,InstantiationException,IllegalAccessException{
+        String controlador="com.mysql.jdbc.Driver";
+        Class.forName(controlador).newInstance();
+        conectar();
+    }
+    public void conectar() throws SQLException{
+        String URL_bd="jdbc:mysql://localhost:3306/sigi_users?useUnicode=true&characterEncoding=utf-8";
+        String user="root";
+        String password = "dedox132";
+        con=DriverManager.getConnection(URL_bd,user,password);
+        sentSQL=con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+    }
+    public ResultSet Consulta(String sql) throws SQLException{ 
+        rst=sentSQL.executeQuery(sql);
+        return rst;
+    }
+    public void Cerrar() throws SQLException {
+        this.con.close();
+    }
+}
