d7c21f6034ce093c52970e857d9be4fce178701e
Diego <diego.mayorga86@gmail.com>
2016-08-04 22:08:19 -0300

    Add more information

--- src/sigi/AyudaAdmin.java
+++ src/sigi/AyudaAdmin.java
@@ -14,6 +14,12 @@ public class AyudaAdmin extends javax.swing.JDialog {
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTextArea3 = new javax.swing.JTextArea();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -24,8 +30,26 @@ public class AyudaAdmin extends javax.swing.JDialog {
         jTextArea1.setColumns(20);
         jTextArea1.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         jTextArea1.setRows(5);
-        jTextArea1.setText("En este modulo se realizan las ventas, para las cuales se puede utilizar un lector de codigo de barras o buscar los articulos por nombre.\nAtajos de teclado:\nF1 -> Venta en Efectivo\nF2 -> Venta en Cuenta Corriente\nF3 -> Ingresar cantidad de un producto por unidad\nF4 -> Ingresar cantidad de un producto pesable\nF5 -> Ingresar valor de un descuento aplicado a un producto");
+        jTextArea1.setText("En este modulo se realizan las ventas, para las cuales se puede utilizar un lector de codigo de barras o buscar los articulos por nombre.\nAtajos de teclado:\nF1 -> Venta en Efectivo\nF2 -> Venta en Cuenta Corriente\nF3 -> Ingresar cantidad de un producto por unidad\nF4 -> Ingresar cantidad de un producto pesable\nF5 -> Ingresar valor de un descuento aplicado a un producto\n\nBuscar Artículos:\nPara buscar un producto por nombre se debe hacer clic en Buscar Articulos, se abrira una nueva ventana. Ahí buscaremos los articulos por\nnombres. Al ir introduciendo letras en la tabla de resultado nos irán apareciendo coinsidencias. Una vez encontrado el artículo que queremos\nagregar debemos hacer clic sobre dicha fila y el articulo se agregará automáticamente al listado de ventas.\n\n");
         jScrollPane1.setViewportView(jTextArea1);
+
+        jLabel2.setFont(new java.awt.Font("Arial", 1, 16)); // NOI18N
+        jLabel2.setText("Anular Ticket");
+
+        jTextArea2.setColumns(20);
+        jTextArea2.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jTextArea2.setRows(5);
+        jTextArea2.setText("Al anular un ticket, se deben volver a ingresar los pruductos al stock. Esto no hace automáticamente por una cuestión de seguridad.\n\nEn el caso de que el ticket anulado corresponda a alguna cuenta corriente, el valor del ticket se tiene que restar manualmente en la \ncuenta corriente correspondiente, ingresando en Cuentas Corrientes -> ABM Cuentas Corrientes.");
+        jScrollPane2.setViewportView(jTextArea2);
+
+        jTextArea3.setColumns(20);
+        jTextArea3.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jTextArea3.setRows(5);
+        jTextArea3.setText("Al usar la herramienta para importar tanto usuarios como articulos, se tiene que usar el formato de excel dado, debe estar guardado\ncomo Excel 97 que genera un archivo .xls.\n\nEn el caso de Importar usuarios, no se deben dejar en blanco los campos Telefono, Celular, Fecha de Nacimiento y Fecha de Alta.\nEn los primieros 2 casos con colocar un cero basta. Para las fechas porner alguna fecha en el siguiente formato 2016-08-02.");
+        jScrollPane3.setViewportView(jTextArea3);
+
+        jLabel3.setFont(new java.awt.Font("Arial", 1, 16)); // NOI18N
+        jLabel3.setText("Importar/Exportar");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -34,6 +58,10 @@ public class AyudaAdmin extends javax.swing.JDialog {
             .addGroup(layout.createSequentialGroup()
                 .addGap(25, 25, 25)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 769, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 769, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2)
                     .addComponent(jLabel1)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 769, javax.swing.GroupLayout.PREFERRED_SIZE)))
         );
@@ -43,7 +71,16 @@ public class AyudaAdmin extends javax.swing.JDialog {
                 .addGap(25, 25, 25)
                 .addComponent(jLabel1)
                 .addGap(11, 11, 11)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(119, Short.MAX_VALUE))
         );
 
         pack();
@@ -93,7 +130,13 @@ public class AyudaAdmin extends javax.swing.JDialog {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JTextArea jTextArea3;
     // End of variables declaration//GEN-END:variables
 }
